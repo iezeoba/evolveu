bff896a2f3ba19264c893d03f7236bce67d3bb42
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-05-31 17:10:27 -0600

    renamed bankapp directory

---
+++ bankapp/src/scripts/account.js
@@ -0,0 +1,31 @@
+export class Account {
+    constructor(name, balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    deposit(amt) {
+        //let amount = parseFloat(amt);
+        if (amt < 0) {
+            // console.log(`Deposit: ${this.name} new balance is ${this.balance}`);
+            return "Amount entered is negative";
+        } else return (this.balance += amt).toFixed(2);
+    }
+
+    withdraw(amt) {
+        //let amount = parseFloat(amt);
+        if (amt > 0 && this.balance - amt >= 0) {
+            // console.log(`Withdraw: ${this.name} new balance is ${this.balance}`);
+            return (this.balance -= amt).toFixed(2);
+        } else if (amt < 0) {
+            return "Amount is negative"
+        } else {
+            return "Insufficient balance";
+        }
+    }
+
+    balance() {
+        return this.balance;
+    }
+};
+// export {Account, AccountController}; //can also export classes this way
