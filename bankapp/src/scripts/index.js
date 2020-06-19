de5e7df814a2c0c698fff98472c7edd250a85d15
Iynaefi <47215909+Iynaefi@users.noreply.github.com>
2020-06-18 22:59:14 -0600

    added react starter files

--- bankapp/src/scripts/index.js
+++ bankapp/src/scripts/index.js
@@ -1,4 +1,4 @@
-import {Account} from "./account.js"
+import { Account } from "./account.js"
 
 const checkingAccount = new Account(acctName.value, 0);
 let newUser = {};
@@ -6,35 +6,90 @@ let createAcct = document.getElementById("createAcct");
 createAcct.addEventListener("click", newCard, newAccount);
 
 function newCard() {
-const div = document.createElement("div");
-div.id = "acctCard";
-div.classList.add("clAcctInfoCard");
+    console.log("seeee");
+    const div = document.createElement("div");
+    div.id = "acctCard";
+    div.classList.add("clAcctInfoCard");
 
-const name = document.createElement("p");
-name.textContent = "Name: " + "acctName.value";
-div.append(name);
+    const name = document.createElement("p");
+    name.id = "idName"; 
+    name.textContent = "Name: " + acctName.value;
+    div.append(name);
 
-const type = document.createElement("p");
-type.textContent = "Type: " + "acctType.value" 
-name.append(type);
+    const type = document.createElement("p");
+    type.id = "idType";
+    type.textContent = "Type: " + dropdown.value;
+    name.append(type);
 
-const balance = document.createElement("p");
-balance.textContent = "Balance: " + "this.balance" 
-type.append(balance);
+    const balance = document.createElement("p");
+    balance.id = "idBal";
+    balance.textContent = "Balance: " + 0.00;
+    type.append(balance);
 
-document.getElementById("acctCard").appendChild(div);
+    document.getElementById("acctCard").appendChild(div);
 }
 
 function newAccount() {
     console.log("testing new acct");
     console.log(Object.keys(newUser));
     // ---------- USER DOES NOT EXIST AND HAS NO ACCOUNT ----------
-if (Object.keys(newUser).includes("name") === false) {
-    newUser.name = acctName.value;
-    newUser.balance = 0;
-}
+    if (Object.keys(newUser).includes("name") === false) {
+        if (dropdown.value == "Checking Account") {
+            newUser.name = acctName.value; //"name" key is declared here on the fly to represent the "name" prop (from the constructor) and initialized with the user input from "acctName.value".
+            newUser.CheckingAccount = 0; //"checkingAccount" key is declared here on the fly to represent the "balance" prop (from the constructor) and initialized with a value of zero. It also references the particular instance of {Account}.
+        } else if (dropdown.value == "Savings Account") {
+            newUser.name = acctName.value;
+            newUser.SavingsAccount = 0;
+        } else if (dropdown.value == "Credit Card") {
+            newUser.name = acctName.value;
+            newUser.CreditCard = 0;
+        } else if (dropdown.value == "RESP Account") {
+            newUser.name = acctName.value;
+            newUser.RESPAccount = 0;
+        } else {
+            newUser.name = acctName.value;
+            newUser[`${dropdown.lastChild.value}`] = 0; // newUser
+        }
+    }
     // ---------- USER EXISTS AND HAS AN ACCOUNT ---------- 
-    //  else if () {
+    else if (Object.keys(newUser).includes("name") && (newUser.name == acctName.value)) {
+        if ((Object.keys(newUser).includes("CheckingAccount") === true) && (dropdown.value === "Checking Account")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("CheckingAccount") === false) && (dropdown.value === "Checking Account")) {
+            newUser.CheckingAccount = 0;
+        }
+        if ((Object.keys(newUser).includes("SavingsAccount") === true) && (dropdown.value === "Savings Account")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("SavingsAccount") === false) && (dropdown.value === "Savings Account")) {
+            newUser.SavingsAccount = 0;
+        }
+        if ((Object.keys(newUser).includes("CreditCard") === true) && (dropdown.value === "Credit Card")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("CreditCard") === false) && (dropdown.value === "Credit Card")) {
+            newUser.CreditCard = 0;
+        }
+        if ((Object.keys(newUser).includes("RESPAccount") === true) && (dropdown.value === "RESP Account")) {
+            alert("User account already exists");
+        } else if ((Object.keys(newUser).includes("RESPAccount") === false) && (dropdown.value === "RESP Account")) {
+            newUser.RESPAccount = 0;
+        }
+    }
+    updateDisplay();
+};
 
-    //  }
+function updateDisplay() {
+    if (Object.keys(newUser).includes("CheckingAccount")) {
+        idName.value = newUser.name;
+        idType.value = dropdown.value;
+        idBal.value = newUser.CheckingAccount;
+    }
+    // else if (Object.keys(newUser).includes("SavingsAccount")) {
+    //     savingsAcc.value = newUser.SavingsAccount;
+    // }
+    // else if (Object.keys(newUser).includes("CreditCard")) {
+    //     creditCard.value = newUser.CreditCard;
+    // }
+    // else if (Object.keys(newUser).includes("RESPAccount")) {
+    //     respAcc.value = newUser.RESPAccount;
+    // }
 }
